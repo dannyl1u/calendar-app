06bd32f2a9bd526528f92aecb601c8bbcca9bb6f
dla216 <danny_liu_5@sfu.ca>
2023-03-07 19:26:57 -0800

    remove unused imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import Calendar from 'react-calendar'
 
 function App() {
+  const [value, onChange] = useState(new Date());
+
   return (
-    <Calendar />
+    <div>
+      <Calendar onChange={onChange} value={value} />
+    </div>
   );
 }
 
